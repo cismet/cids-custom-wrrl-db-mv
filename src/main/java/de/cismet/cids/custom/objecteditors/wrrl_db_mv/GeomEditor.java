10a40991e685dad4301552e735cf3785e5d3c45b
Jean Ruiz <jean.ruiz@cismet.de>
2010-12-20 12:59:16 +0000

    [issue:1800] GeomEditor

---
+++ src/main/java/de/cismet/cids/custom/objecteditors/wrrl_db_mv/GeomEditor.java
@@ -0,0 +1,11 @@
+package de.cismet.cids.custom.objecteditors.wrrl_db_mv;
+
+import de.cismet.cismap.cids.geometryeditor.DefaultCismapGeometryComboBoxEditor;
+
+/**
+ *
+ * @author jruiz
+ */
+public class GeomEditor extends DefaultCismapGeometryComboBoxEditor {
+
+}
