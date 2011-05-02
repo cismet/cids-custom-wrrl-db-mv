5f1c8afe70e94f0f571ce7184c96142e2bd0668f
Jean Ruiz <jean.ruiz@cismet.de>
2011-05-02 10:06:40 +0000

    [issue:2052] Bewirtschaftungsende - toString()-Methode mit Wasserkörperkennung statt der ID

--- src/main/java/de/cismet/cids/custom/tostringconverter/wrrl_db_mv/BewirtschaftungsendeToStringConverter.java
+++ src/main/java/de/cismet/cids/custom/tostringconverter/wrrl_db_mv/BewirtschaftungsendeToStringConverter.java
@@ -7,17 +7,6 @@
 ****************************************************/
 package de.cismet.cids.custom.tostringconverter.wrrl_db_mv;
 
-import Sirius.navigator.connection.SessionManager;
-
-import Sirius.server.middleware.types.MetaClass;
-import Sirius.server.middleware.types.MetaObject;
-
-import de.cismet.cids.custom.util.CidsBeanSupport;
-
-import de.cismet.cids.dynamics.CidsBean;
-
-import de.cismet.cids.navigator.utils.ClassCacheMultiple;
-
 import de.cismet.cids.tools.CustomToStringConverter;
 
 /**
@@ -28,37 +17,10 @@ import de.cismet.cids.tools.CustomToStringConverter;
  */
 public class BewirtschaftungsendeToStringConverter extends CustomToStringConverter {
 
-    //~ Static fields/initializers ---------------------------------------------
-
-    private static final org.apache.log4j.Logger LOG = org.apache.log4j.Logger.getLogger(
-            BewirtschaftungsendeToStringConverter.class);
-
-    //~ Instance fields --------------------------------------------------------
-
-    final MetaClass mcWkFg = ClassCacheMultiple.getMetaClass(CidsBeanSupport.DOMAIN_NAME, "wk_fg");
-
     //~ Methods ----------------------------------------------------------------
 
     @Override
     public String createString() {
-        try {
-            final int wkfgId = Integer.valueOf((String)cidsBean.getProperty("wk_k"));
-
-            final String queryWkFg = "SELECT " + mcWkFg.getID() + ", wk_fg." + mcWkFg.getPrimaryKey() + " "
-                        + "FROM " + mcWkFg.getTableName() + " AS wk_fg "
-                        + "WHERE id = " + wkfgId + ";";
-            final MetaObject[] mosWkFg;
-            if (LOG.isDebugEnabled()) {
-                LOG.debug("queryWkFg  => " + queryWkFg);
-            }
-            mosWkFg = SessionManager.getProxy().getMetaObjectByQuery(queryWkFg, 0);
-            final CidsBean wkfgBean = mosWkFg[0].getBean();
-            return "Bewirtschaftungsende auf " + String.valueOf(wkfgBean.getProperty("wk_k"));
-        } catch (Exception ex) {
-            if (LOG.isDebugEnabled()) {
-                LOG.debug("error while creating string", ex);
-            }
-            return "Bewirtschaftungsende auf ...";
-        }
+        return "Bewirtschaftungsende für " + String.valueOf(cidsBean.getProperty("wk_k"));
     }
 }
