9d4c4729f2fc9ccbcf315e5dab1eb4bbf58879ad
Thorsten Herter <thorsten.herter@cismet.de>
2011-05-13 11:35:50 +0000

    [issue:0002052] Maßnahmen Renderer auch für umgesetzte Maßnahmen nutzen

---
+++ src/main/java/de/cismet/cids/custom/featurerenderer/wrrl_db_mv/MassnahmenUmsetzungFeatureRenderer.java
@@ -0,0 +1,17 @@
+/***************************************************
+*
+* cismet GmbH, Saarbruecken, Germany
+*
+*              ... and it just works.
+*
+****************************************************/
+package de.cismet.cids.custom.featurerenderer.wrrl_db_mv;
+
+/**
+ * DOCUMENT ME!
+ *
+ * @author   therter
+ * @version  $Revision$, $Date$
+ */
+public class MassnahmenUmsetzungFeatureRenderer extends MassnahmenFeatureRenderer {
+}
