d91c6cc6f713bde0c8c4578e4f5ebc43d9896aef
Thorsten Herter <thorsten.herter@cismet.de>
2011-10-20 08:16:40 +0000

    [issue:0002198] toString-Konverter korrigiert

--- src/main/java/de/cismet/cids/custom/tostringconverter/wrrl_db_mv/GupGewaesserabschnittToStringConverter.java
+++ src/main/java/de/cismet/cids/custom/tostringconverter/wrrl_db_mv/GupGewaesserabschnittToStringConverter.java
@@ -25,21 +25,8 @@ public class GupGewaesserabschnittToStringConverter extends CustomToStringConver
 
     @Override
     public String createString() {
-        final CidsBean statVonBean = (CidsBean)cidsBean.getProperty("linie."
-                        + LinearReferencingConstants.PROP_STATIONLINIE_FROM);
-        final CidsBean statBisBean = (CidsBean)cidsBean.getProperty("linie."
-                        + LinearReferencingConstants.PROP_STATIONLINIE_TO);
-        final Long gwk = (Long)cidsBean.getProperty("linie."
-                        + LinearReferencingConstants.PROP_STATIONLINIE_FROM + "."
-                        + LinearReferencingConstants.PROP_STATION_ROUTE + "."
-                        + LinearReferencingConstants.PROP_ROUTE_GWK);
-        final Double von = (Double)statVonBean.getProperty(LinearReferencingConstants.PROP_STATION_VALUE);
-        final Double bis = (Double)statBisBean.getProperty(LinearReferencingConstants.PROP_STATION_VALUE);
-
-        final String gwkString = (gwk == null) ? "unbekannt" : String.valueOf(gwk);
-        final String vonString = (von == null) ? "unbekannt" : String.format("%06d", von.intValue());
-        final String bisString = (bis == null) ? "unbekannt" : String.format("%06d", bis.intValue());
-
-        return gwkString + " [" + vonString + " - " + bisString + "]";
+        String name = (cidsBean == null ? "unbenannt" : String.valueOf( cidsBean.getProperty("name")) );
+        
+        return name;
     }
 }
