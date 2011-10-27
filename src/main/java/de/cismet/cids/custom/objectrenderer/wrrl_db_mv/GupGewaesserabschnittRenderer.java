3ab1d38410e439633e0f8c032a153d448c2bdb05
Thorsten Herter <thorsten.herter@cismet.de>
2011-10-27 15:50:35 +0000

    [issue:0002198]  GupGewaesserabschnittRenderer

---
+++ src/main/java/de/cismet/cids/custom/objectrenderer/wrrl_db_mv/GupGewaesserabschnittRenderer.java
@@ -0,0 +1,19 @@
+/***************************************************
+*
+* cismet GmbH, Saarbruecken, Germany
+*
+*              ... and it just works.
+*
+****************************************************/
+package de.cismet.cids.custom.objectrenderer.wrrl_db_mv;
+
+import de.cismet.cids.custom.objecteditors.wrrl_db_mv.GupGewaesserabschnittEditor;
+
+/**
+ * DOCUMENT ME!
+ *
+ * @author   therter
+ * @version  $Revision$, $Date$
+ */
+public class GupGewaesserabschnittRenderer extends GupGewaesserabschnittEditor {
+}
