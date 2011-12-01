6a56a302a56f0781f00cbc74f74e7eb7ee2fe3d9
Thorsten Hell <thorsten.hell@cismet.de>
2011-12-01 08:38:02 +0000

    [issue:2341] fixed GeomEditor erweitert jetzt direkt DefaultCismapGeometryComboBoxEditor

---
+++ src/main/java/de/cismet/cids/custom/objecteditors/wrrl_db_mv/GeomEditor.java
@@ -0,0 +1,23 @@
+/***************************************************
+*
+* cismet GmbH, Saarbruecken, Germany
+*
+*              ... and it just works.
+*
+****************************************************/
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package de.cismet.cids.custom.objecteditors.wrrl_db_mv;
+
+import de.cismet.cismap.cids.geometryeditor.DefaultCismapGeometryComboBoxEditor;
+
+/**
+ * DOCUMENT ME!
+ *
+ * @author   thorsten
+ * @version  $Revision$, $Date$
+ */
+public class GeomEditor extends DefaultCismapGeometryComboBoxEditor {
+}
