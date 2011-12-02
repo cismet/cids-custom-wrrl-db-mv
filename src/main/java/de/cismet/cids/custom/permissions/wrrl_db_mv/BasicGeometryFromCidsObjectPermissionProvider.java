b4de97e11274c02c0f0c2b19989bae453cc53908
Thorsten Hell <thorsten.hell@cismet.de>
2011-12-02 10:42:27 +0000

    [issue:2343] CustomObjectPermissions hinzugefuegt

---
+++ src/main/java/de/cismet/cids/custom/permissions/wrrl_db_mv/BasicGeometryFromCidsObjectPermissionProvider.java
@@ -0,0 +1,113 @@
+/***************************************************
+*
+* cismet GmbH, Saarbruecken, Germany
+*
+*              ... and it just works.
+*
+****************************************************/
+package de.cismet.cids.custom.permissions.wrrl_db_mv;
+
+import Sirius.server.newuser.User;
+
+import com.vividsolutions.jts.geom.Geometry;
+import com.vividsolutions.jts.index.strtree.STRtree;
+
+import java.sql.Timestamp;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+
+import de.cismet.cids.dynamics.AbstractCustomBeanPermissionProvider;
+import de.cismet.cids.dynamics.CidsBean;
+
+import de.cismet.cismap.navigatorplugin.CidsFeature;
+
+/**
+ * DOCUMENT ME!
+ *
+ * @author   thorsten
+ * @version  $Revision$, $Date$
+ */
+public abstract class BasicGeometryFromCidsObjectPermissionProvider extends AbstractCustomBeanPermissionProvider {
+
+    //~ Static fields/initializers ---------------------------------------------
+
+    private static final transient org.apache.log4j.Logger log = org.apache.log4j.Logger.getLogger(
+            BasicGeometryFromCidsObjectPermissionProvider.class);
+
+    //~ Methods ----------------------------------------------------------------
+
+    @Override
+    public boolean getCustomReadPermissionDecisionforUser(final User u) {
+        return true;
+    }
+
+    @Override
+    public boolean getCustomWritePermissionDecisionforUser(final User u) {
+        if (u.getUserGroup().getName().equalsIgnoreCase("administratoren")) {
+            if (log.isDebugEnabled()) {
+                log.debug("member of admin group. permission is granted");
+            }
+            return true;
+        }
+
+        final Geometry objectGeom = getGeometry();
+        if (objectGeom == null) {
+            log.info(getGeometry() + "delivered a null value. access is granted");
+            return true;
+        }
+
+        final String userGroup = u.getUserGroup().getName();
+
+        final STRtree restrictionfeatures = CidsRestrictionGeometryStore.getRestrictions().get(userGroup);
+        final ArrayList<String> restrictionKeys = CidsRestrictionGeometryStore.getRestrictionKeys().get(userGroup);
+
+        boolean keyWordHitsAvailable = false;
+
+        for (final String keyentry : restrictionKeys) {
+            if (keyentry.toLowerCase().indexOf(getKey().toLowerCase()) > -1) {
+                keyWordHitsAvailable = true;
+                break;
+            }
+        }
+
+        final List selectedFeatures = restrictionfeatures.query(objectGeom.getEnvelopeInternal());
+        for (final Object hit : selectedFeatures) {
+            final CidsFeature cf = (CidsFeature)hit;
+            final Geometry hitGeom = cf.getGeometry();
+            final CidsBean cb = cf.getMetaObject().getBean();
+            final Timestamp ts = (Timestamp)cb.getProperty("validuntil");
+            final Date now = new Date();
+            final String restrictionkeys = (String)cb.getProperty("restrictionkeys");
+
+            final boolean geometryHit = hitGeom.contains(objectGeom);
+            final boolean keyWordHit = restrictionkeys.toLowerCase().indexOf(getKey().toLowerCase()) > -1;
+            final boolean timestampHit = ((ts == null) || (ts.getTime() > now.getTime()));
+
+            if (keyWordHit && geometryHit && timestampHit) {
+                return true;
+            }
+        }
+        if (keyWordHitsAvailable) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    /**
+     * DOCUMENT ME!
+     *
+     * @return  DOCUMENT ME!
+     */
+    public abstract Geometry getGeometry();
+
+    /**
+     * DOCUMENT ME!
+     *
+     * @return  DOCUMENT ME!
+     */
+    public abstract String getKey();
+}
---
+++ src/main/java/de/cismet/cids/custom/permissions/wrrl_db_mv/CidsRestrictionGeometryStore.java
@@ -0,0 +1,146 @@
+/***************************************************
+*
+* cismet GmbH, Saarbruecken, Germany
+*
+*              ... and it just works.
+*
+****************************************************/
+package de.cismet.cids.custom.permissions.wrrl_db_mv;
+
+import Sirius.navigator.connection.SessionManager;
+
+import Sirius.server.middleware.types.MetaClass;
+import Sirius.server.middleware.types.MetaObject;
+
+import com.vividsolutions.jts.index.strtree.STRtree;
+
+import org.openide.util.lookup.ServiceProvider;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import de.cismet.cids.client.tools.DevelopmentTools;
+
+import de.cismet.cids.dynamics.CidsBean;
+
+import de.cismet.cids.navigator.utils.ClassCacheMultiple;
+
+import de.cismet.cismap.navigatorplugin.CidsFeature;
+
+import de.cismet.tools.configuration.StartupHook;
+
+/**
+ * DOCUMENT ME!
+ *
+ * @author   thorsten
+ * @version  $Revision$, $Date$
+ */
+@ServiceProvider(service = StartupHook.class)
+public class CidsRestrictionGeometryStore implements StartupHook {
+
+    //~ Static fields/initializers ---------------------------------------------
+
+    public static final String DOMAIN = "WRRL_DB_MV";
+    public static final String TABLE = "CCS_RESTRICTIONINFO";
+    private static final transient org.apache.log4j.Logger log = org.apache.log4j.Logger.getLogger(
+            CidsRestrictionGeometryStore.class);
+    private static HashMap<String, STRtree> restrictions = new HashMap<String, STRtree>();
+    private static HashMap<String, ArrayList<String>> restrictionKeys = new HashMap<String, ArrayList<String>>();
+
+    //~ Methods ----------------------------------------------------------------
+
+    @Override
+    public void applicationStarted() {
+        try {
+            if (log.isDebugEnabled()) {
+                log.fatal("CidsRestrictionGeometryStore initialization started");
+            }
+            final MetaClass mc = ClassCacheMultiple.getMetaClass(DOMAIN, TABLE);
+            final MetaObject[] metaObjects = SessionManager.getConnection()
+                        .getMetaObjectByQuery(SessionManager.getSession().getUser(),
+                            "SELECT "
+                            + mc.getID()
+                            + ", "
+                            + mc.getPrimaryKey()
+                            + " FROM "
+                            + mc.getTableName()
+                            + " order by "
+                            + mc.getPrimaryKey());
+
+            for (final MetaObject mo : metaObjects) {
+                final CidsBean cb = mo.getBean();
+
+                final String group = (String)cb.getProperty("usergroup.name");
+                STRtree restrictionfeatures = restrictions.get(group);
+                ArrayList<String> grouprestrictionKeys = restrictionKeys.get(group);
+
+                if (restrictionfeatures == null) {
+                    restrictionfeatures = new STRtree();
+                    restrictions.put(group, restrictionfeatures);
+                }
+
+                if (grouprestrictionKeys == null) {
+                    grouprestrictionKeys = new ArrayList<String>();
+                    restrictionKeys.put(group, grouprestrictionKeys);
+                }
+
+                final CidsFeature feature = new CidsFeature(mo);
+                restrictionfeatures.insert(feature.getGeometry().getEnvelopeInternal(), feature);
+                grouprestrictionKeys.add((String)cb.getProperty("restrictionkeys"));
+            }
+            if (log.isDebugEnabled()) {
+                log.debug("CidsRestrictionGeometryStore initialized with " + metaObjects.length + " Object"
+                            + ((metaObjects.length == 1) ? "" : "s") + ".");
+            }
+        } catch (Exception e) {
+            log.warn("Error during initialization of restriction objects.", e);
+        }
+    }
+
+    /**
+     * DOCUMENT ME!
+     *
+     * @return  DOCUMENT ME!
+     */
+    public static HashMap<String, STRtree> getRestrictions() {
+        return restrictions;
+    }
+
+    /**
+     * DOCUMENT ME!
+     *
+     * @return  DOCUMENT ME!
+     */
+    public static HashMap<String, ArrayList<String>> getRestrictionKeys() {
+        return restrictionKeys;
+    }
+
+    /**
+     * DOCUMENT ME!
+     *
+     * @param   args  DOCUMENT ME!
+     *
+     * @throws  Exception  DOCUMENT ME!
+     */
+    public static void main(final String[] args) throws Exception {
+        final CidsBean[] cbs = DevelopmentTools.createCidsBeansFromRMIConnectionOnLocalhost(
+                DOMAIN,
+                "Administratoren",
+                "admin",
+                "sb",
+                TABLE);
+        for (final CidsBean cb : cbs) {
+            log.fatal(cb.getMOString());
+            final CidsBean cbX = DevelopmentTools.createCidsBeanFromRMIConnectionOnLocalhost(
+                    DOMAIN,
+                    "Administratoren",
+                    "admin",
+                    "sb",
+                    "oeg_einzugsgebiet",
+                    1);
+            final CidsBean g = (CidsBean)cbX.getProperty("geom");
+            cb.setProperty("restrictiongeom", g);
+            log.fatal(cb.getMOString());
+        }
+    }
+}
