14aac93fbacd156c1013d0c237c49a9b8d469804
therter <thorsten.herter@cismet.de>
2012-10-04 19:22:21 +0200

    #2 some changes of the los editor/renderer and introduction of the poi editor/renderer

---
+++ src/main/java/de/cismet/cids/custom/objectrenderer/wrrl_db_mv/GupPoiRouteRenderer.java
@@ -0,0 +1,32 @@
+/***************************************************
+*
+* cismet GmbH, Saarbruecken, Germany
+*
+*              ... and it just works.
+*
+****************************************************/
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package de.cismet.cids.custom.objectrenderer.wrrl_db_mv;
+
+import de.cismet.cids.custom.objecteditors.wrrl_db_mv.GupPoiRouteEditor;
+
+/**
+ * DOCUMENT ME!
+ *
+ * @author   therter
+ * @version  $Revision$, $Date$
+ */
+public class GupPoiRouteRenderer extends GupPoiRouteEditor {
+
+    //~ Constructors -----------------------------------------------------------
+
+    /**
+     * Creates a new GupPoiRouteRenderer object.
+     */
+    public GupPoiRouteRenderer() {
+        super(true);
+    }
+}
