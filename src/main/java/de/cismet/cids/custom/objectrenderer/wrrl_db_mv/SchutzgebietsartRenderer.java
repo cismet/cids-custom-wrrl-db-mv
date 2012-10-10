0365b51bb00ff98564deba7605f378ce03956dd9
therter <thorsten.herter@cismet.de>
2012-10-10 18:58:08 +0200

    #2 schutzgebiete, massnahmenart, lose

---
+++ src/main/java/de/cismet/cids/custom/objectrenderer/wrrl_db_mv/SchutzgebietsartRenderer.java
@@ -0,0 +1,32 @@
+/***************************************************
+*
+* cismet GmbH, Saarbruecken, Germany
+*
+*              ... and it just works.
+*
+****************************************************/
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package de.cismet.cids.custom.objectrenderer.wrrl_db_mv;
+
+import de.cismet.cids.custom.objecteditors.wrrl_db_mv.SchutzgebietsartEditor;
+
+/**
+ * DOCUMENT ME!
+ *
+ * @author   therter
+ * @version  $Revision$, $Date$
+ */
+public class SchutzgebietsartRenderer extends SchutzgebietsartEditor {
+
+    //~ Constructors -----------------------------------------------------------
+
+    /**
+     * Creates a new SchutzgebietsartRenderer object.
+     */
+    public SchutzgebietsartRenderer() {
+        super(true);
+    }
+}
