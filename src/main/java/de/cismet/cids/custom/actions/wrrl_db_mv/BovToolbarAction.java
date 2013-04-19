5996ca6eed877098c4fe6e76a440264fc1d43a3f
therter <thorsten.herter@cismet.de>
2013-04-19 16:16:33 +0200

    #25 improvements of the status workflow

--- src/main/java/de/cismet/cids/custom/actions/wrrl_db_mv/BovToolbarAction.java
+++ src/main/java/de/cismet/cids/custom/actions/wrrl_db_mv/BovToolbarAction.java
@@ -1,44 +1,20 @@
-/***************************************************
-*
-* cismet GmbH, Saarbruecken, Germany
-*
-*              ... and it just works.
-*
-****************************************************/
 /*
- *  Copyright (C) 2011 thorsten
- *
- *  This program is free software: you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation, either version 3 of the License, or
- *  (at your option) any later version.
- *
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *
- *  You should have received a copy of the GNU General Public License
- *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
  */
 package de.cismet.cids.custom.actions.wrrl_db_mv;
 
-import org.openide.util.lookup.ServiceProvider;
-
 import de.cismet.cids.custom.wrrl_db_mv.commons.WRRLUtil;
-
 import de.cismet.cids.navigator.utils.AbstractNewObjectToolbarAction;
 import de.cismet.cids.navigator.utils.CidsClientToolbarItem;
+import org.openide.util.lookup.ServiceProvider;
 
 /**
- * DOCUMENT ME!
  *
- * @author   thorsten
- * @version  $Revision$, $Date$
+ * @author therter
  */
-
 @ServiceProvider(service = CidsClientToolbarItem.class)
-public class BewirtschaftungsendeToolbarAction extends AbstractNewObjectToolbarAction {
+public class BovToolbarAction extends AbstractNewObjectToolbarAction {
 
     //~ Methods ----------------------------------------------------------------
 
@@ -54,11 +30,11 @@ public class BewirtschaftungsendeToolbarAction extends AbstractNewObjectToolbarA
 
     @Override
     public String getTableName() {
-        return "bewirtschaftungsende";
+        return "bov";
     }
 
     @Override
     public String getTooltipString() {
-        return "neues Bewirtschaftungsende anlegen";
+        return "neue BOV anlegen";
     }
 }
