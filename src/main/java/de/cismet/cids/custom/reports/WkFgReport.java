d234c236548c3a3af6b0fd83d67e724d8d4bf59d
therter <thorsten.herter@cismet.de>
2014-12-16 14:16:34 +0100

    #78 new wk-fg report

--- src/main/java/de/cismet/cids/custom/reports/WkFgReport.java
+++ src/main/java/de/cismet/cids/custom/reports/WkFgReport.java
@@ -7,13 +7,22 @@
 ****************************************************/
 package de.cismet.cids.custom.reports;
 
+import Sirius.navigator.connection.Connection;
+import Sirius.navigator.connection.ConnectionFactory;
+import Sirius.navigator.connection.ConnectionInfo;
+import Sirius.navigator.connection.ConnectionSession;
 import Sirius.navigator.connection.SessionManager;
+import Sirius.navigator.connection.proxy.ConnectionProxy;
 import Sirius.navigator.exception.ConnectionException;
 
 import Sirius.server.middleware.types.MetaClass;
 import Sirius.server.middleware.types.MetaObject;
+import Sirius.server.newuser.UserException;
 
 import org.mortbay.log.Log;
+
+import java.io.FileReader;
+import java.io.Reader;
 
 import java.text.DecimalFormat;
 
@@ -22,10 +31,15 @@ import java.util.Collection;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
-
-import de.cismet.cids.client.tools.DevelopmentTools;
+import java.util.Properties;
+import java.util.concurrent.Executor;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 import de.cismet.cids.custom.wrrl_db_mv.commons.WRRLUtil;
+import de.cismet.cids.custom.wrrl_db_mv.server.search.TeilgebieteSearch;
+import de.cismet.cids.custom.wrrl_db_mv.server.search.WkFgIdSearch;
 import de.cismet.cids.custom.wrrl_db_mv.util.CidsBeanSupport;
 import de.cismet.cids.custom.wrrl_db_mv.util.LawaTableModel;
 import de.cismet.cids.custom.wrrl_db_mv.util.TeileComparator;
@@ -34,9 +48,15 @@ import de.cismet.cids.dynamics.CidsBean;
 
 import de.cismet.cids.navigator.utils.ClassCacheMultiple;
 
+import de.cismet.cids.server.search.CidsServerSearch;
+
 import de.cismet.cids.utils.jasperreports.ReportSwingWorker;
 
+import de.cismet.cismap.commons.gui.MappingComponent;
+import de.cismet.cismap.commons.gui.layerwidget.ActiveLayerModel;
 import de.cismet.cismap.commons.interaction.CismapBroker;
+
+import de.cismet.tools.configuration.ConfigurationManager;
 
 import de.cismet.tools.gui.StaticSwingTools;
 
@@ -51,6 +71,7 @@ public class WkFgReport {
     //~ Static fields/initializers ---------------------------------------------
 
     private static final org.apache.log4j.Logger LOG = org.apache.log4j.Logger.getLogger(WkFgReport.class);
+    private static ConfigurationManager configManager = new ConfigurationManager();
 
     //~ Methods ----------------------------------------------------------------
 
@@ -88,6 +109,106 @@ public class WkFgReport {
     }
 
     /**
+     * Creates the report for the given cids bean and saves the file in the given directory.
+     *
+     * @param  directory  the directory to save the reports
+     * @param  cidsBean   the report will be created for this wk_fg bean
+     * @param  executor   the report thread will be executed with this executor, if it is not null
+     */
+    public static void createReport(final String directory, final CidsBean cidsBean, final Executor executor) {
+        final Collection<CidsBean> coll = new ArrayList<CidsBean>();
+        coll.add(cidsBean);
+
+        final ArrayList<Collection<CidsBean>> beans = new ArrayList<Collection<CidsBean>>();
+        beans.add(coll);
+        beans.add(getMassnahmen((Integer)cidsBean.getProperty("id")));
+        beans.add(getMassnahmenUmgesetzt((Integer)cidsBean.getProperty("id")));
+
+        final ArrayList<String> reports = new ArrayList<String>();
+        reports.add("/de/cismet/cids/custom/reports/wk_fg.jasper");
+        reports.add("/de/cismet/cids/custom/reports/wk_fg_massnahmen.jasper");
+        reports.add("/de/cismet/cids/custom/reports/wk_fg_massnahmenUmgesetzt.jasper");
+
+        final HashMap parameters = new HashMap();
+        parameters.put("STATIONIERUNGEN", getStationierungen(cidsBean));
+        parameters.put("GEWAESSERKENNZAHLEN", getGewaesserkennzahlen(cidsBean));
+        parameters.put("LAWA-DETAILTYP", getLawaDetailTyp(cidsBean));
+        parameters.put("BEWIRTSCHAFTUNGSBEREICHE", getBewirtschaftungsbereiche(cidsBean));
+
+        final String reportName = String.valueOf(cidsBean.getProperty("wk_k"));
+
+        final ReportSwingWorker worker = new ReportSwingWorker(
+                beans,
+                reports,
+                false,
+                null,
+                directory,
+                parameters,
+                reportName);
+
+        if (executor == null) {
+            worker.execute();
+        } else {
+            executor.execute(worker);
+        }
+    }
+
+    /**
+     * Creates all wk_fg reports and saves the files in the given directory.
+     *
+     * @param  directory   the directory to save the reports
+     * @param  expression  DOCUMENT ME!
+     */
+    public static void createAllReports(final String directory, final String expression) {
+        final MetaClass wkFgMc = ClassCacheMultiple.getMetaClass(WRRLUtil.DOMAIN_NAME, "wk_fg");
+        final String query = "SELECT " + wkFgMc.getID() + ", " + wkFgMc.getPrimaryKey() + " FROM "
+                    + wkFgMc.getTableName();
+        final ArrayList ids = new ArrayList();
+
+        try {
+            final CidsServerSearch search = new WkFgIdSearch(expression);
+            final Collection res = SessionManager.getProxy()
+                        .customServerSearch(SessionManager.getSession().getUser(), search);
+            final ArrayList<ArrayList> resArray = (ArrayList<ArrayList>)res;
+
+            if ((resArray != null) && (resArray.size() > 0) && (resArray.get(0).size() > 0)) {
+                for (final ArrayList l : resArray) {
+                    ids.add(l.get(0));
+                }
+            }
+        } catch (ConnectionException e) {
+        }
+
+        final ExecutorService executor = Executors.newFixedThreadPool(5);
+//        final ExecutorService executor = CismetExecutors.newCachedThreadPool();
+
+//        if (expression != null) {
+//            query += " WHERE wk_k ilike '" + expression + "'";
+//        }
+
+        try {
+            for (final Object id : ids) {
+                final Integer oId = (Integer)id;
+                final MetaObject wkFgObject = SessionManager.getProxy()
+                            .getMetaObject(oId.intValue(), wkFgMc.getID(), WRRLUtil.DOMAIN_NAME);
+                createReport(directory, wkFgObject.getBean(), executor);
+            }
+        } catch (Exception e) {
+            LOG.error("Error while creating all wk_fg reports", e);
+            System.exit(1);
+        }
+
+        try {
+            executor.shutdown();
+            executor.awaitTermination(10, TimeUnit.DAYS);
+        } catch (InterruptedException e) {
+            // nothing to do
+        }
+
+        System.exit(0);
+    }
+
+    /**
      * DOCUMENT ME!
      *
      * @param   id  DOCUMENT ME!
@@ -105,14 +226,42 @@ public class WkFgReport {
                         + "   " + mcMassnahmen.getTableName() + " "
                         + "WHERE "
                         + "   wk_fg = " + String.valueOf(id)
+                        + " and massn_fin = false "
                         + "ORDER BY massn_id"
                         + ";";
 
             return getBeansFromQuery(query);
         } catch (Exception ex) {
-            if (LOG.isDebugEnabled()) {
-                LOG.fatal("", ex);
-            }
+            LOG.error("Error while getting massnahmen for wk-fg with id " + String.valueOf(id), ex);
+            return null;
+        }
+    }
+
+    /**
+     * DOCUMENT ME!
+     *
+     * @param   id  DOCUMENT ME!
+     *
+     * @return  DOCUMENT ME!
+     */
+    public static Collection<CidsBean> getMassnahmenUmgesetzt(final int id) {
+        try {
+            final MetaClass mcMassnahmen = ClassCacheMultiple.getMetaClass(WRRLUtil.DOMAIN_NAME, "massnahmen");
+
+            final String query = "SELECT "
+                        + "   " + mcMassnahmen.getID() + ", "
+                        + "   " + mcMassnahmen.getPrimaryKey() + " "
+                        + "FROM "
+                        + "   " + mcMassnahmen.getTableName() + " "
+                        + "WHERE "
+                        + "   wk_fg = " + String.valueOf(id)
+                        + " and massn_fin = true "
+                        + "ORDER BY massn_id"
+                        + ";";
+
+            return getBeansFromQuery(query);
+        } catch (Exception ex) {
+            LOG.error("Error while getting massnahmen umgesetzt for wk-fg with id " + String.valueOf(id), ex);
             return null;
         }
     }
@@ -186,15 +335,21 @@ public class WkFgReport {
         final int anteilIndex = 1;
         final int amountDifferentTypes = model.getRowCount();
 
-        String lawaDetailTyp = "";
+        String lawaDetailTyp = null;
 
         for (int i = 0; i < (amountDifferentTypes - 1); i++) {
             final String typ = (String)model.getValueAt(i, typIndex);
             final String typ_number = typ.split("-")[0];
+            final String typ_name = typ.substring(typ.indexOf("-") + 1);
             final String anteil = (String)model.getValueAt(i, anteilIndex);
-            lawaDetailTyp += typ_number + " (" + anteil + "%), ";
+
+            if (lawaDetailTyp == null) {
+                lawaDetailTyp = typ_name + " (Typ " + typ_number + ", " + anteil + "% der Länge), ";
+            } else {
+                lawaDetailTyp += "<br />" + typ_name + " (Typ " + typ_number + ", " + anteil + "% der Länge), ";
+            }
         }
-        if (!lawaDetailTyp.equals("")) {
+        if ((lawaDetailTyp != null) && !lawaDetailTyp.equals("")) {
             lawaDetailTyp = lawaDetailTyp.substring(0, lawaDetailTyp.length() - 2);
         }
 
@@ -282,9 +437,8 @@ public class WkFgReport {
 
             return getBeansFromQuery(query);
         } catch (Exception ex) {
-            if (LOG.isDebugEnabled()) {
-                LOG.fatal("", ex);
-            }
+            LOG.error("Error while getting bewirtschaftungsende for wk-fg teil: " + String.valueOf(teilBean), ex);
+
             return null;
         }
     }
@@ -296,18 +450,87 @@ public class WkFgReport {
      */
     public static void main(final String[] args) {
         try {
-            DevelopmentTools.createRendererInFrameFromRMIConnectionOnLocalhost(
-                "WRRL_DB_MV",
-                "Administratoren",
-                "admin",
-                "kif",
-                "wk_fg",
-                23,
-                "Test",
-                1280,
-                1024);
+            String expression = null;
+
+            if (args.length == 0) {
+                System.out.println("Als Parameter muss der Pfad zur Konfigurationsdatei angegeben werden");
+                System.exit(1);
+            }
+
+            if (args.length == 2) {
+                expression = args[1];
+            }
+
+//            expression = "WANE-0300";
+
+            // read the properties
+            final Properties properties = new Properties();
+//            final Reader propertiesReader = new FileReader(
+//                    "/home/therter/ApplicationData/flexDist/client/wrrl-db-mvLocalMv/config/wkReports.properties");
+            final Reader propertiesReader = new FileReader(args[0]);
+            properties.load(propertiesReader);
+
+            // login
+            final ConnectionInfo connectionInfo = new ConnectionInfo();
+            connectionInfo.setCallserverURL(properties.getProperty("callserver"));
+            connectionInfo.setPassword(properties.getProperty("password"));
+            connectionInfo.setUserDomain(properties.getProperty("userDomain"));
+            connectionInfo.setUsergroup(properties.getProperty("userGroup"));
+            connectionInfo.setUsergroupDomain(properties.getProperty("userGroupDomain"));
+            connectionInfo.setUsername(properties.getProperty("username"));
+
+            final Connection connection = ConnectionFactory.getFactory()
+                        .createConnection(properties.getProperty("connectionClass"),
+                            properties.getProperty("callserver"),
+                            null);
+            final ConnectionSession session;
+            final ConnectionProxy proxy;
+
+            try {
+                session = ConnectionFactory.getFactory().createSession(connection, connectionInfo, true);
+                proxy = ConnectionFactory.getFactory()
+                            .createProxy("Sirius.navigator.connection.proxy.DefaultConnectionProxyHandler", session);
+                SessionManager.init(proxy);
+            } catch (UserException uexp) {
+                LOG.error("autologin failed", uexp); // NOI18N
+                System.exit(1);
+            }
+
+            initCismap();
+
+            // init log4J
+            final Properties p = new Properties();
+            p.put("log4j.appender.Console", "org.apache.log4j.ConsoleAppender");   // NOI18N
+            p.put("log4j.appender.Console.layout", "org.apache.log4j.TTCCLayout"); // NOI18N
+            p.put("log4j.rootLogger", "ERROR,Console");                            // NOI18N
+            org.apache.log4j.PropertyConfigurator.configure(p);
+
+            // create the reports
+            createAllReports(properties.getProperty("report_directory"), expression);
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    /**
+     * Initialises the MappingComponent.
+     */
+    private static void initCismap() {
+        final MappingComponent mappingComponent = new MappingComponent();
+
+        configManager.setDefaultFileName("de/cismet/cids/custom/reports/defaultCismapProperties.xml");
+        configManager.setFallBackFileName("de/cismet/cids/custom/reports/defaultCismapProperties.xml");
+
+        final ActiveLayerModel mappingModel = new ActiveLayerModel();
+        configManager.addConfigurable((ActiveLayerModel)mappingModel);
+        configManager.addConfigurable(mappingComponent);
+
+        configManager.configure(mappingModel);
+        mappingComponent.preparationSetMappingModel(mappingModel);
+        configManager.configure(mappingComponent);
+
+        mappingComponent.setMappingModel(mappingModel);
+
+        CismapBroker.getInstance().setMappingComponent(mappingComponent);
     }
 }
--- src/main/java/de/cismet/cids/custom/reports/scriptlets/WkFgScriptlet.java
+++ src/main/java/de/cismet/cids/custom/reports/scriptlets/WkFgScriptlet.java
@@ -13,6 +13,7 @@ import Sirius.navigator.exception.ConnectionException;
 import Sirius.server.middleware.types.MetaClass;
 import Sirius.server.middleware.types.MetaObject;
 
+import com.vividsolutions.jts.geom.Geometry;
 import com.vividsolutions.jts.geom.GeometryFactory;
 import com.vividsolutions.jts.geom.LineString;
 
@@ -22,30 +23,33 @@ import net.sf.jasperreports.engine.JRScriptletException;
 import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
 import net.sf.jasperreports.engine.fill.JRFillField;
 
-import org.openide.util.Exceptions;
-
-import java.awt.Graphics;
+import java.awt.Color;
 import java.awt.Graphics2D;
 import java.awt.Image;
 import java.awt.image.BufferedImage;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 import java.util.ListIterator;
 import java.util.concurrent.locks.Condition;
 import java.util.concurrent.locks.Lock;
-import java.util.concurrent.locks.ReentrantLock;
 
 import de.cismet.cids.custom.wrrl_db_mv.commons.WRRLUtil;
+import de.cismet.cids.custom.wrrl_db_mv.server.search.TeilgebieteSearch;
 import de.cismet.cids.custom.wrrl_db_mv.util.CidsBeanSupport;
 
 import de.cismet.cids.dynamics.CidsBean;
 
 import de.cismet.cids.navigator.utils.ClassCacheMultiple;
 
-import de.cismet.cismap.commons.BoundingBox;
+import de.cismet.cids.server.search.CidsServerSearch;
+
 import de.cismet.cismap.commons.HeadlessMapProvider;
 import de.cismet.cismap.commons.XBoundingBox;
+import de.cismet.cismap.commons.features.DefaultStyledFeature;
+import de.cismet.cismap.commons.features.FeatureGroups;
+import de.cismet.cismap.commons.features.PureFeatureGroup;
 import de.cismet.cismap.commons.raster.wms.simple.SimpleWMS;
 import de.cismet.cismap.commons.raster.wms.simple.SimpleWmsGetMapUrl;
 import de.cismet.cismap.commons.retrieval.RetrievalEvent;
@@ -107,9 +111,8 @@ public class WkFgScriptlet extends JRDefaultScriptlet {
                         + ";";
             return getBeansFromQuery(query);
         } catch (Exception ex) {
-            if (LOG.isDebugEnabled()) {
-                LOG.fatal("", ex);
-            }
+            LOG.error("Error while getting self for wk-fg with id " + String.valueOf(getId()), ex);
+
             return null;
         }
     }
@@ -134,11 +137,75 @@ public class WkFgScriptlet extends JRDefaultScriptlet {
 
             return getBeansFromQuery(query);
         } catch (Exception ex) {
-            if (LOG.isDebugEnabled()) {
-                LOG.fatal("", ex);
-            }
+            LOG.error("Error while getting massnahmen for wk-fg with id " + String.valueOf(getId()), ex);
+
             return null;
         }
+    }
+
+    /**
+     * DOCUMENT ME!
+     *
+     * @return  DOCUMENT ME!
+     */
+    public String getTeilgebiet() {
+        final GeometryFactory gf = new GeometryFactory();
+        final Collection<CidsBean> wkTeile = (Collection<CidsBean>)((JRFillField)fieldsMap.get("teile")).getValue();
+        final Collection<LineString> lineStrings = new ArrayList<LineString>();
+        for (final CidsBean wkTeilBean : wkTeile) {
+            final CidsBean geomBean = (CidsBean)wkTeilBean.getProperty("linie.geom");
+            final LineString geom = (LineString)geomBean.getProperty("geo_field");
+            lineStrings.add(geom);
+        }
+        final Geometry wkGeom = gf.createMultiLineString(
+                lineStrings.toArray(new LineString[0]));
+
+        try {
+            final CidsServerSearch search = new TeilgebieteSearch(wkGeom.toText());
+            final Collection res = SessionManager.getProxy()
+                        .customServerSearch(SessionManager.getSession().getUser(), search);
+            final ArrayList<ArrayList> resArray = (ArrayList<ArrayList>)res;
+
+            if ((resArray != null) && (resArray.size() > 0) && (resArray.get(0).size() > 0)) {
+                final Object o = resArray.get(0).get(0);
+
+                if (o instanceof String) {
+                    return o.toString();
+                }
+            }
+        } catch (ConnectionException e) {
+            LOG.error("Exception during a cids server search.", e); // NOI18N
+        }
+
+        return null;
+    }
+
+    /**
+     * DOCUMENT ME!
+     *
+     * @return  DOCUMENT ME!
+     */
+    public String getGewName() {
+        final Collection<CidsBean> wkTeile = (Collection<CidsBean>)((JRFillField)fieldsMap.get("teile")).getValue();
+        final List<String> gewNames = new ArrayList();
+        String allNames = null;
+
+        for (final CidsBean wkTeilBean : wkTeile) {
+            final String gewName = (String)wkTeilBean.getProperty("linie.von.route.routenname");
+            if (!gewNames.contains(gewName)) {
+                gewNames.add(gewName);
+            }
+        }
+
+        for (final String name : gewNames) {
+            if (allNames == null) {
+                allNames = name;
+            } else {
+                allNames += ", " + name;
+            }
+        }
+
+        return allNames;
     }
 
     /**
@@ -176,9 +243,8 @@ public class WkFgScriptlet extends JRDefaultScriptlet {
 
             return lawa_types;
         } catch (Exception ex) {
-            if (LOG.isDebugEnabled()) {
-                LOG.fatal("Problem in getLawa() in WkFgScriplet", ex);
-            }
+            LOG.error("Error while getting lawa types for wk-fg with id " + String.valueOf(getId()), ex);
+
             return null;
         }
     }
@@ -264,14 +330,12 @@ public class WkFgScriptlet extends JRDefaultScriptlet {
                         + "   m.messstelle = s.id AND "
                         + "   s.wk_fg = " + getId() + " "
                         + "ORDER BY "
-                        + "   messjahr DESC "
-                        + "LIMIT 1;"; // NOI18N
+                        + "   messjahr DESC ";
 
             return getBeansFromQuery(query);
         } catch (final Exception ex) {
-            if (LOG.isDebugEnabled()) {
-                LOG.fatal("", ex);
-            }
+            LOG.error("Error while getting mst for wk-fg with id " + String.valueOf(getId()), ex);
+
             return null;
         }
     }
@@ -342,8 +406,8 @@ public class WkFgScriptlet extends JRDefaultScriptlet {
                         + "&TRANSPARENT=TRUE"
                         + "&BGCOLOR=0xF0F0F0"
                         + "&EXCEPTIONS=application/vnd.ogc.se_xml"
-                        + "&LAYERS=route_stat,biomst,chemmst,wk_fg"
-                        + "&STYLES=default,default,default,default";
+                        + "&LAYERS=wk_fg,report_route_stat"
+                        + "&STYLES=default,default";
             final GeometryFactory gf = new GeometryFactory();
             final Collection<CidsBean> wkTeile = (Collection<CidsBean>)((JRFillField)fieldsMap.get("teile")).getValue();
             final Collection<LineString> lineStrings = new ArrayList<LineString>();
@@ -367,6 +431,84 @@ public class WkFgScriptlet extends JRDefaultScriptlet {
             getMapUrl = new SimpleWmsGetMapUrl(urlOverlay);
             simpleWms = new SimpleWMS(getMapUrl);
             mapProvider.addLayer(simpleWms);
+            final DefaultStyledFeature f = new DefaultStyledFeature();
+            f.setGeometry(gf.createMultiLineString(lineStrings.toArray(new LineString[0])));
+            f.setHighlightingEnabled(true);
+            f.setPrimaryAnnotation("WANE-0300");
+            f.setPrimaryAnnotationVisible(true);
+            f.setPrimaryAnnotationPaint(Color.BLACK);
+            f.setPrimaryAnnotationHalo(Color.WHITE);
+            f.setAutoScale(true);
+            f.setLinePaint(Color.RED);
+            f.setLineWidth(3);
+            mapProvider.addFeature(f);
+
+            return mapProvider.getImageAndWait(72, 130, 555, 375);
+        } catch (Exception e) {
+            LOG.error("Error while retrievin gmap.", e);
+            return null;
+        }
+    }
+
+    /**
+     * generates the map in the WkFg Report. The map consists of a background and some layers, which are merged.
+     *
+     * @return  DOCUMENT ME!
+     */
+    public Image generateOverviewMap() {
+        try {
+            final String urlBackground = "http://www.geodaten-mv.de/dienste/gdimv_topomv"
+                        + "?REQUEST=GetMap&VERSION=1.1.1&SERVICE=WMS&LAYERS=gdimv_topomv"
+                        + "&BBOX=<cismap:boundingBox>"
+                        + "&SRS=EPSG:35833&FORMAT=image/png"
+                        + "&WIDTH=<cismap:width>"
+                        + "&HEIGHT=<cismap:height>"
+                        + "&STYLES=&EXCEPTIONS=application/vnd.ogc.se_inimage";
+//            final String urlOverlay = "http://wms.fis-wasser-mv.de/services?&VERSION=1.1.1"
+//                        + "&REQUEST=GetMap"
+//                        + "&BBOX=<cismap:boundingBox>"
+//                        + "&WIDTH=<cismap:width>"
+//                        + "&HEIGHT=<cismap:height>"
+//                        + "&SRS=EPSG:35833&FORMAT=image/png"
+//                        + "&TRANSPARENT=TRUE"
+//                        + "&BGCOLOR=0xF0F0F0"
+//                        + "&EXCEPTIONS=application/vnd.ogc.se_xml"
+//                        + "&LAYERS=wk_fg,report_route_stat"
+//                        + "&STYLES=default,default";
+            final GeometryFactory gf = new GeometryFactory();
+            final Collection<CidsBean> wkTeile = (Collection<CidsBean>)((JRFillField)fieldsMap.get("teile")).getValue();
+            final Collection<LineString> lineStrings = new ArrayList<LineString>();
+            for (final CidsBean wkTeilBean : wkTeile) {
+                final CidsBean geomBean = (CidsBean)wkTeilBean.getProperty("linie.geom");
+                final LineString geom = (LineString)geomBean.getProperty("geo_field");
+                lineStrings.add(geom);
+            }
+            final XBoundingBox boundingBox = new XBoundingBox(gf.createMultiLineString(
+                        lineStrings.toArray(new LineString[0])));
+            boundingBox.setX1(boundingBox.getX1() - 50);
+            boundingBox.setY1(boundingBox.getY1() - 50);
+            boundingBox.setX2(boundingBox.getX2() + 50);
+            boundingBox.setY2(boundingBox.getY2() + 50);
+
+            final HeadlessMapProvider mapProvider = new HeadlessMapProvider();
+            mapProvider.setBoundingBox(boundingBox);
+            final SimpleWmsGetMapUrl getMapUrl = new SimpleWmsGetMapUrl(urlBackground);
+            final SimpleWMS simpleWms = new SimpleWMS(getMapUrl);
+            mapProvider.addLayer(simpleWms);
+//            getMapUrl = new SimpleWmsGetMapUrl(urlOverlay);
+//            simpleWms = new SimpleWMS(getMapUrl);
+//            mapProvider.addLayer(simpleWms);
+            final DefaultStyledFeature f = new DefaultStyledFeature();
+            f.setGeometry(gf.createMultiLineString(lineStrings.toArray(new LineString[0])));
+            f.setHighlightingEnabled(true);
+            f.setPrimaryAnnotation("WANE-0300");
+            f.setPrimaryAnnotationVisible(true);
+            f.setPrimaryAnnotationPaint(Color.BLACK);
+            f.setPrimaryAnnotationHalo(Color.WHITE);
+            f.setAutoScale(true);
+            f.setLinePaint(Color.RED);
+            f.setLineWidth(3);
+            mapProvider.addFeature(f);
 
             return mapProvider.getImageAndWait(72, 130, 555, 375);
         } catch (Exception e) {
